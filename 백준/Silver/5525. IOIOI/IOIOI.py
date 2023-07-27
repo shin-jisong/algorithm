42c885e67516c94828843df1acfda155ae7d8925
gmuz1c <86762272+shin-jisong@users.noreply.github.com>
2023-07-27 16:34:59 +0900

    [Silver I] Title: IOIOI, Time: 116 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -5,24 +5,11 @@ m = int(sys.stdin.readline().rstrip())
 s = sys.stdin.readline().rstrip()
 
 sLen = 2 * n + 1
-
-def check(s):
-    ch = 'I'
-    ans = True
-    for i in s:
-        if i != ch:
-            ans = False
-            break
-        if ch == 'I':
-            ch = 'O'
-        else:
-            ch = 'I'
-    return ans
-
 res = 0
+checkS = "I" + "OI" * n
 for i in range(m):
     if i + sLen <= m:
-        if check(s[i:i+sLen]):
+        if s[i:i+sLen] == checkS:
             res += 1
 
 print(res)
