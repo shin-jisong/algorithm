5d806ce458363985a392eec12034e19331b32b79
gmuz1c <songjin1127@naver.com>
2024-10-30 10:16:18 +0900

    [Bronze V] Title: A-B, Time: 108 ms, Memory: 14388 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String line = reader.readLine();
+        String[] words = line.split("\\s+");
+        int a = Integer.parseInt(words[0]);
+        int b = Integer.parseInt(words[1]);
+        System.out.println(a - b);
+    }
+}
